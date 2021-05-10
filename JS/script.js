248bf17e2a116e1a1b085ed077e6d5f2aad384b6
freesudani <mohamed.dahiya@hotmail.com>
2021-05-10 18:09:41 +0700

    JS

--- JS/script.js
+++ JS/script.js
@@ -4,10 +4,12 @@ const message = document.querySelector('.message');
 const number = document.querySelector('.number');
 const scoretext = document.querySelector('.score');
 const check = document.querySelector('.check');
+const again = document.querySelector('.again');
 
 let score = 20;
-const RandomNumber = Math.trunc(Math.random() * 20) + 1;
-number.textContent = RandomNumber;
+let RandomNumber = Math.trunc(Math.random() * 20) + 1;
+let highscore = 0;
+
 
 check.addEventListener('click', function() {
   const guess = Number(document.querySelector('.guess').value);
@@ -16,6 +18,18 @@ check.addEventListener('click', function() {
     message.textContent = 'No number!';
   } else if (guess === RandomNumber) {
       message.textContent = 'Correct Number!';
+      
+      number.textContent = RandomNumber;
+
+      document.querySelector('body').style.backgroundColor = '#60b347';
+
+      document.querySelector('.number').style.width = '30rem'; 
+
+      if(score > highscore) {
+        highscore = score;
+        document.querySelector(.'highscore').textContent = highscore;
+      }
+
   } else if (guess !== RandomNumber) {
     if (score > 1) {
         message.textContent = (guess > RandomNumber) ? 'Too high': 'Too low';
@@ -25,4 +39,16 @@ check.addEventListener('click', function() {
         message.textContent = 'You lost the game!';
     }    
   }
+});
+
+
+again.addEventListener('click' , function() {
+  score = 20;
+  RandomNumber = Math.trunc(Math.random() * 20) + 1;
+  message.textContent = 'Start guessing...';
+  scoretext.textContent = score;
+  number.textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.guess').value = ' ';
+  document.querySelector('.number').style.width = '15rem'; 
 });
